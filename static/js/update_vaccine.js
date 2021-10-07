3af3a47e4cc74ba6eab3b866284d784212dfaa0e
Mihir Karandikar <mihirkkarandikar@gmail.com>
2021-10-07 20:07:30 +0530

    Add CSS files

---
+++ static/js/update_vaccine.js
@@ -0,0 +1,17 @@
+console.log("Vaccine!!!");
+// let table_cell_1 = document.getElementById('v1');
+
+// table_cell_1.addEventListener('click',()=>{
+//     console.log("Click Event Fired")
+//     let html = table_cell_1.innerText;
+//     table_cell_1.innerHTML = `<textarea class="v1_quantity form-control" id="v1_quantity" rows="1">${html}</textarea>`;
+    
+//     let updateVal = document.getElementById('v1_quantity');
+//     updateVal.addEventListener('blur',()=>{
+//         console.log("Blur Event Fired")
+//         let html = table_cell_1.innerText;
+//         table_cell_1.innerHTML = `<td>${html}</td>`;
+//     }) 
+
+//     console.log("After Everything")
+// })
