0812fb057f421f9c9e8a0b6fe28f03f81b384040
Mihir Karandikar <mihirkkarandikar@gmail.com>
2021-10-19 14:28:12 +0530

    Delete Scripts

--- MySQL Scripts/hospital_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `Hospital` (
-  `hosp_ID` INT,
-  `hospName` varchar(255),
-  `hospEmail` varchar(255),
-  `hospPhone` INT,
-  `hospAddress` varchar(255),
-  `hospPassword` varchar(255),
-  PRIMARY KEY (`hosp_ID`));
-
